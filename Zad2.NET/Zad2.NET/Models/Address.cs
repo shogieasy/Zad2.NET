261c66dd8c91d4d71da8c12de569806687c256d4
shogi <shogi@DESKTOP-DCG1AN7>
2021-03-15 19:14:37 +0100

    zad2

---
+++ Zad2.NET/Zad2.NET/Models/Address.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zad2.NET.Models
+{
+    public class Address
+    {
+
+        [Display(Name = "Twoja ulubiona ulica")]
+        [StringLength(60,MinimumLength = 3), Required(ErrorMessage = "Polejest obowiązkowe")]
+        
+
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public int Number { get; set; }
+    }
+
+}
--- Zad2.NET/Zad2.NET/Pages/Index.cshtml.cs
+++ Zad2.NET/Zad2.NET/Pages/Index.cshtml.cs
@@ -5,12 +5,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Zad2.NET.Models;
 
 namespace Zad2.NET.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public Address Address { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        
+
+        public string Name { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,7 +28,23 @@ namespace Zad2.NET.Pages
 
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+                
+
+            }
 
         }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        } 
     }
+
 }
